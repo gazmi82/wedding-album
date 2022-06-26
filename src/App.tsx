6f4652136ebca290521c835075a28e26bdd743b2
gazmir82 <gazmir.sulcaj@gis-dev.com>
2022-06-26 23:44:05 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-center">Wedding app</h1>
+  );
+}
+
+export default App;
